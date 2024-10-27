01dc67d6c8b8862d71e6b705e61f2ca8df916626
Rao Waleed <rao.waleed.nadeem@gmail.com>
2024-10-27 12:09:38 +0500

    Problem 705

---
+++ Leetcode 2/705. Design HashSet.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+//class MyHashSet {
+//    vector<int>v;
+//public:
+//    MyHashSet() {
+//
+//    }
+//
+//    void add(int key) {
+//        v.push_back(key);
+//    }
+//
+//    void remove(int key) {
+//        auto it = find(v.begin(), v.end(), key);
+//        if (it != v.end()) {
+//            v.erase(it);
+//        }
+//    }
+//
+//    bool contains(int key) {
+//        auto it = find(v.begin(), v.end(), key);
+//        if (it != v.end()) {
+//            return true;
+//        }
+//        return false;
+//    }
+//};
+//
+//int main()
+//{
+//	system("pause");
+//	return 0;
+//}
