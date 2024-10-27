c1893d77489eeb9471afb5f7bbecdb20ced42346
Rao Waleed <rao.waleed.nadeem@gmail.com>
2024-10-27 16:32:36 +0500

    Problem 75

---
+++ Leetcode 2/75. Sort Colors.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//class Solution {
+//public:
+//    void sortColors(vector<int>& nums) {
+//        sort(nums.begin(), nums.end());
+//    }
+//};
+//int main()
+//{
+//	system("pause");
+//	return 0;
+//}
